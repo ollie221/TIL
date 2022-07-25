8b2e9c85fa014b2d2b70f6c14e568c073048822d
BOYOUNG-KANG <28287@naver.com>
2022-07-26 01:09:21 +0900

    boolean

---
+++ Java_Flow_Control/BooleanApp.java
@@ -0,0 +1,18 @@
+
+public class BooleanApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println(true);
+		System.out.println(false);
+		
+		String foo = "Hello World";
+//		String true = "Hello World"; 
+//      true와 false처럼 이미 컴퓨터 언어에서 쓰임이 있는 것들(=reserved word) 변수화x
+	
+		System.out.println(foo.contains("World"));
+		System.out.println(foo.contains("bbo"));
+		
+	}
+
+}
