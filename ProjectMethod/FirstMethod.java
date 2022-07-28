98d6f2adec103cf47c15ab31e1436c4625134f99
BOYOUNG-KANG <28287@naver.com>
2022-07-29 02:11:59 +0900

    method(=function):복잡한 것을 단순하게 만드는 정리정돈의 도구

---
+++ ProjectMethod/FirstMethod.java
@@ -0,0 +1,14 @@
+
+public class FirstMethod {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Method");   //들어온 입력값을 모니터에 출력해주는 메소드
+		System.out.println(Math.floor(1.1)); //입력값을 받아서 입력값 내림하여 출력하는 메소드
+		//floor내림, ceil올림 
+
+	}
+//main이라는 이름 가진 메소드.
+//class실행시킬때 반드시 약속된 이름인 main이라는 메소드를 실행해야함so실행하고자하는 명령문을 본문(main) 안에 위치시킴
+
+}
