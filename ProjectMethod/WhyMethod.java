89cb37200448e3b76c58f2d9c33b7e7a987b91ce
BOYOUNG-KANG <28287@naver.com>
2022-07-31 01:39:51 +0900

    method출력

--- ProjectMethod/WhyMethod.java
+++ ProjectMethod/WhyMethod.java
@@ -6,13 +6,20 @@ public class WhyMethod {
 		
 					// 인자, argument
 		printTwoTimes("a","-");//-aa
+		System.out.println (twoTimes("a", "-"));
 		// 100000000
 		printTwoTimes("a","*");//*aa
 		// 100000000
 		printTwoTimes("a","&");//&aa
 		printTwoTimes("b","!");//!bb
 	}
-	
+	public static String twoTimes(String text, String delimiter) {
+		String out ="";
+		out = out + delimiter + "\n";
+		out = out + text + "\n";
+		out = out + text + "\n";
+		return out;
+	}	
 //printTwoTimes라는 method를 정의
 //이 method의 괄호 안 첫번째 값은 반드시 String
 //이렇게 들어온 값은 중괄호 안에서 text라는 이름의 변수의 값이 되겠구나
